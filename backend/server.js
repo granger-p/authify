4c8d7b1bc2ca998ce33026a007ddfe0415e64b38
granger-p <pritimaheshwari2512@gmail.com>
2026-02-16 12:03:15 +0530

    Setting up the database

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,26 @@
-const server= async () => {
-    console.log("server is running..");
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connect } from 'mongoose';
+import connectDB from './db/connect.js';
+dotenv.config();
+const port = process.env.PORT || 8000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const server = async () => {
+try {
+  await connect(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+    process.exit(1);
+  }
 };
+
 server();
